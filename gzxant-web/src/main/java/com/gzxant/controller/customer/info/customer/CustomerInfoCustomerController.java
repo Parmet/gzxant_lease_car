b95aa8359267aa6791465e3fb155fe407a5979c2
tecty <619058900@qq.com>
2018-07-11 18:14:36 +0800

    A demo of vue implementation.

--- gzxant-web/src/main/java/com/gzxant/controller/customer/info/customer/CustomerInfoCustomerController.java
+++ gzxant-web/src/main/java/com/gzxant/controller/customer/info/customer/CustomerInfoCustomerController.java
@@ -36,7 +36,7 @@ import javax.validation.Valid;
 
 /**
  * <p>
- * Customer's information 前端控制器
+ * Customer's information 控制器
  * </p>
  *
  * @author tecty
@@ -52,8 +52,15 @@ public class CustomerInfoCustomerController extends BaseController {
 	@Autowired
 	private ICustomerInfoCertificateService iCustomerInfoCertificateService;
 
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		// this binder is for successfully transfer from String to Date
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		dateFormat.setLenient(true);
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+	}
 
-	private Map getReturnMap(Map basicMap){
+	private Map<String,Object> getReturnMap(Map<String, Object> basicMap){
 		// Fetch dict information dependency then inject it into basic map.
 		basicMap.putAll(iSysDictService.getDictMapByMap( new HashMap<String, String>(){{
 			put("driveCapability", "DriveCapability");
@@ -61,15 +68,6 @@ public class CustomerInfoCustomerController extends BaseController {
 			put("status", "CustomerStatus");
 		}}));
 		return basicMap;
-	}
-
-
-	@InitBinder
-	public void initBinder(WebDataBinder binder) {
-		// this binder is for successfully transfer from String to Date
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		dateFormat.setLenient(true);
-		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
 	}
 
 	/**
@@ -91,16 +89,15 @@ public class CustomerInfoCustomerController extends BaseController {
 	@GetMapping("")
 	public Map get(@RequestParam(value = "page",defaultValue = "1") int page,
 				   @RequestParam(value = "page_size",defaultValue = "10") int pageSize ){
-		Map returnMap = new HashMap();
+		Map<String,Object> returnMap = new HashMap<>();
 		// put the customer's list
 		returnMap.put("customers",
 					customerInfoCustomerService.selectPage(
-						new Page(page,pageSize)
+						new Page<CustomerInfoCustomer>(page,pageSize)
 					).getRecords()
 		);
 
-
-		return this.getReturnMap(returnMap);
+		return getReturnMap(returnMap);
 	}
 
 	/**
@@ -110,7 +107,8 @@ public class CustomerInfoCustomerController extends BaseController {
 	 */
 	@GetMapping("/{id}")
 	public Map get(@PathVariable("id") Long id ){
-		Map returnMap = new HashMap();
+		Map<String,Object> returnMap = new HashMap<>();
+		// get an empty instance
 		returnMap.put("customer", customerInfoCustomerService.selectById(id));
 		return this.getReturnMap(returnMap);
 	}
@@ -122,8 +120,7 @@ public class CustomerInfoCustomerController extends BaseController {
 	@PutMapping("")
 	public ReturnDTO update(@RequestBody @Valid CustomerInfoCustomer customer){
 		// call update by id and store the new customer's information
-        System.out.println(customer.toString());
-		customerInfoCustomerService.update(customer, Condition.create().eq("id", customer.getId()));
+		customerInfoCustomerService.updateById(customer);
 		return  ReturnDTOUtil.success();
 	}
 
@@ -136,73 +133,4 @@ public class CustomerInfoCustomerController extends BaseController {
 		customerInfoCustomerService.deleteById(id);
 		return ReturnDTOUtil.success();
 	}
-
-
-
-/*
-	Legacy code, Shifting to the rest style.
- */
-//	@ApiOperation(value = "进入Customer's information列表界面", notes = "进入Customer's information列表界面")
-//	@GetMapping(value = "")
-//	public String list(Model model) {
-//		return "/customer/info/customer/list";
-//	}
-//
-//	@ApiOperation(value = "进入Customer's information编辑界面", notes = "进入Customer's information编辑界面")
-//	@GetMapping(value = "/detail/{action}")
-//	public String detail(@PathVariable("action") String action, Model model) {
-//		model.addAttribute("action", action);
-//		return "/customer/info/customer/detail";
-//	}
-//
-//	@ApiOperation(value = "进入Customer's information创建界面", notes = "进入Customer's information创建界面")
-//	@GetMapping("/create")
-//	public String create(Model model){
-//		model.addAttribute("typeList",iSysDictService.getDictTree("CertificateType"));
-//		model.addAttribute("statusList",iSysDictService.getDictTree("CustomerStatus"));
-//		model.addAttribute("capabilityList",iSysDictService.getDictTree("DriveCapability"));
-//		model.addAttribute("genderList",iSysDictService.getDictTree("sex"));
-//		return "/customer/info/customer/insert";
-//	}
-//
-//	@ApiOperation(value = "获取Customer's information列表数据", notes = "获取Customer's information列表数据:使用约定的DataTable")
-//	@PostMapping(value = "/list")
-//	@ResponseBody
-//	public DataTable<CustomerInfoCustomer> list(@RequestBody DataTable<CustomerInfoCustomer> dt) {
-//		return customerInfoCustomerService.pageSearch(dt);
-//	}
-//
-//	@ApiOperation(value = "添加Customer's information", notes = "添加Customer's information")
-//	@PostMapping(value = "/create")
-//	@ResponseBody
-//	public ReturnDTO create(CustomerInfoCustomer customer, CustomerInfoCertificate certificate) {
-//		// insert certificate first then wired the certificate id to the customer
-//		iCustomerInfoCertificateService.insert(certificate);
-//
-//		// wire the id
-//		customer.setCertificateId(certificate.getId());
-//		// store the customer into database
-//		customerInfoCustomerService.insert(customer);
-//		return ReturnDTOUtil.success();
-//	}
-//
-//	@ApiOperation(value = "编辑Customer's information", notes = "编辑Customer's information")
-//	@PostMapping(value = "/update")
-//	@ResponseBody
-//	public ReturnDTO update(CustomerInfoCustomer param) {
-//		customerInfoCustomerService.updateById(param);
-//		return ReturnDTOUtil.success();
-//	}
-//
-//	@SLog("批量删除Customer's information")
-//	@ApiOperation(value = "批量删除Customer's information", notes = "批量删除Customer's information")
-//	@PostMapping(value = "/delete")
-//	@ResponseBody
-//	public ReturnDTO delete(@RequestParam("ids") List<Long> ids) {
-//		boolean success = customerInfoCustomerService.deleteBatchIds(ids);
-//		if (success) {
-//			return ReturnDTOUtil.success();
-//		}
-//		return ReturnDTOUtil.fail();
-//	}
 }
