af4919ba00f50dc287f15b16d98b64579d2377c8
Rainybow <731158143@qq.com>
2018-07-22 22:30:32 +0800

    修改违章管理查询bug

--- gzxant-operation/src/main/java/com/gzxant/dao/transgress/TransgressMapper.java
+++ gzxant-operation/src/main/java/com/gzxant/dao/transgress/TransgressMapper.java
@@ -11,47 +11,49 @@ import com.gzxant.entity.transgress.Transgress;
 
 public interface TransgressMapper {
 	
-	@Select("select * from oper_transgress "
-			/*+"<if test=' tgVo != null'>"
-			+" when "
-			+"</if>"*/
+	@Select({"<script>",
+			"select * from oper_transgress ",
+			"<if test=' tgVo != null'>",
+			" when ",
+			"</if>",
 			/*  资产公司  */
-			/*+"<if test=' asset_company != null and asset_company != \"\" '>"
-			+" and asset_company LIKE CONCAT('%',#{asset_company},'%') "
-			+"</if>"*/
+			"<if test=' asset_company != null and asset_company != \"\" '>",
+			" and asset_company LIKE CONCAT('%',#{asset_company},'%') ",
+			"</if>",
 			/*  经营公司 */
-			/*+"<if test=' operate_company != null and operate_company != \"\" '>"
-			+" and operate_company LIKE CONCAT('%',#{operate_company},'%') "
-			+"</if>"*/
+			"<if test=' operate_company != null and operate_company != \"\" '>",
+			" and operate_company LIKE CONCAT('%',#{operate_company},'%') ",
+			"</if>",
 			/*  客户 */
-			/*+"<if test=' customer != null and customer != \"\" '>"
-			+" and customer LIKE CONCAT('%',#{customer},'%') "
-			+"</if>"*/
+			"<if test=' customer != null and customer != \"\" '>",
+			" and customer LIKE CONCAT('%',#{customer},'%') ",
+			"</if>",
 			/*  车牌号 */
-			/*+"<if test=' plate_num != null and plate_num != \"\" '>"
-			+" and plate_num LIKE CONCAT('%',#{plate_num},'%') "
-			+"</if>"*/
+			"<if test=' plate_num != null and plate_num != \"\" '>",
+			" and plate_num LIKE CONCAT('%',#{plate_num},'%') ",
+			"</if>",
 			/*  状态 */
-			/*+"<if test=' status != null and status != \"\" '>"
-			+" and status = #{status} "
-			+"</if>"*/
+			"<if test=' status != null and status != \"\" '>",
+			" and status = #{status} ",
+			"</if>",
 			/*  违章时间开始 */
-			/*+"<if test=' tg_time_status != null and tg_time_status != \"\" '>"
-			+" and plate_num &gt;= #{tg_time_status} "
-			+"</if>"*/
+			"<if test=' tg_time_status != null and tg_time_status != \"\" '>",
+			" and plate_num &gt;= #{tg_time_status} ",
+			"</if>",
 			/*  违章时间结束 */
-			/*+"<if test=' tg_time_end != null and tg_time_end != \"\" '>"
-			+" and plate_num &lt;= #{tg_time_end} "
-			+"</if>"*/
+			"<if test=' tg_time_end != null and tg_time_end != \"\" '>",
+			" and plate_num &lt;= #{tg_time_end} ",
+			"</if>",
 			/*  处理时间开始 */
-			/*+"<if test=' update_time_status != null and update_time_status != \"\" '>"
-			+" and update_time &gt;= #{update_time_status} "
-			+"</if>"*/
+			"<if test=' update_time_status != null and update_time_status != \"\" '>",
+			" and update_time &gt;= #{update_time_status} ",
+			"</if>",
 			/*  处理时间结束 */
-			/*+"<if test=' update_time_end != null and update_time_end != \"\" '>"
-			+" and update_time &lt;= #{update_time_end} "
-			+"</if>"*/
-			+ "order by query_time")
+			"<if test=' update_time_end != null and update_time_end != \"\" '>",
+			" and update_time &lt;= #{update_time_end} ",
+			"</if>",
+			"order by query_time",
+			"</script>"})
 	List<Transgress> query(TransgressVo tgVo);
 
 	@Insert("insert into oper_transgress(user_id,plate_num,type,brand,deduction,"
