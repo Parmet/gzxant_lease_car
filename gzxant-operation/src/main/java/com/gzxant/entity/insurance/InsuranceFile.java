44a2138fb5c72c3c53ef5b16c124ca7773008502
kcen <736074502@qq.com>
2018-07-27 19:05:34 +0800

    add

---
+++ gzxant-operation/src/main/java/com/gzxant/entity/insurance/InsuranceFile.java
@@ -0,0 +1,26 @@
+package com.gzxant.entity.insurance;
+
+public class InsuranceFile {
+	private String code;
+	private String fileName;
+	private String fileUrl;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getFileUrl() {
+		return fileUrl;
+	}
+	public void setFileUrl(String fileUrl) {
+		this.fileUrl = fileUrl;
+	}
+}
